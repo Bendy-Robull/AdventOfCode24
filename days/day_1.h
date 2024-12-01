8b35d53363d4831c50ac6979c3766bbdccdfe3dd
Bendy-Robull <t.bebe99@gmail.com>
2024-12-01 23:16:53 +0100

    Day 1 Part 2 Done

--- days/day_1.h
+++ days/day_1.h
@@ -1,9 +1,5 @@
 #include "../pch.h"
-
-void partOne(std::string fileName) {
-	std::list<int> firstList;
-	std::list<int> secondList;
-
+void readFile(std::list<int>& listOne, std::list<int>& listTwo, std::string fileName) {
 	std::ifstream inputFile(fileName);
 	std::string line;
 	while (std::getline(inputFile, line)) {
@@ -11,19 +7,24 @@ void partOne(std::string fileName) {
 		int number;
 		int numberTwo;
 		ss >> number >> numberTwo;
-		firstList.push_back(number);
-		secondList.push_back(numberTwo);
+		listOne.push_back(number);
+		listTwo.push_back(numberTwo);
 	}
 	inputFile.close();
+}
+void partOne(std::string fileName) {
+	std::list<int> firstList;
+	std::list<int> secondList;
+	readFile(firstList, secondList, fileName);
 	firstList.sort();
 	secondList.sort();
-	std::cout<< firstList.size() << std::endl;
+	std::cout << firstList.size() << std::endl;
 	int sum = 0;
-	while(!firstList.empty()) {
-		std::cout << firstList.front() << " : " << secondList.front() << std::endl;
+	while (!firstList.empty()) {
+		//std::cout << firstList.front() << " : " << secondList.front() << std::endl;
 		int first = firstList.front();
 		int second = secondList.front();
-		sum += std::abs(first-second);
+		sum += std::abs(first - second);
 		firstList.pop_front();
 		secondList.pop_front();
 	}
@@ -31,5 +32,20 @@ void partOne(std::string fileName) {
 }
 
 void partTwo(std::string fileName) {
-
+	std::list<int> firstList;
+	std::list<int> secondList;
+	readFile(firstList, secondList, fileName);
+	int sum = 0;
+	for (auto i : firstList) {
+		int occurence = 0;
+		for (auto j : secondList) {
+			if (i == j)
+			{
+				occurence++;
+			}
+		}
+		//std::cout << i << " : " << occurence << std::endl;
+		sum += occurence * i;
+	}
+	std::cout << sum << std::endl;
 }
--- pch.h
+++ pch.h
@@ -5,10 +5,12 @@
 #include <fstream>
 #include <sstream>
 #include <list>
+#include <vector>
+#include <map>
 #include <algorithm>
 
 #include "days/day_1.h";
 #define CURRENT_DAY 1
-#define CURRENT_PART 1
+#define CURRENT_PART 2
 #define TEST_DATA false
 
