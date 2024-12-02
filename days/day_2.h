d00be1aa68f95724df8945f6cdf0bdf49858cebc
Bendy-Robull <t.bebe99@gmail.com>
2024-12-02 12:12:06 +0100

    day 2 start

---
+++ days/day_2.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "../pch.h"
+
+void partOne(std::string fileName) {
+
+}
+
+void partTwo(std::string fileName) {
+
+}
--- pch.h
+++ pch.h
@@ -5,7 +5,7 @@
 
 
 #include "days/day_1.h";
+#include "days/day_2.h";
 #define CURRENT_DAY 1
 #define CURRENT_PART 1
 #define TEST_DATA true
-
